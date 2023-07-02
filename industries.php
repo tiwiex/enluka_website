d3398d77aad330c5ddc11b0f130faeeedb7eaff4
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-02 21:32:03 +0100

    Update all industries

--- industries.php
+++ industries.php
@@ -125,10 +125,15 @@ include 'includes/navigation.php';
                     <div class="row overflow-hidden">    
                         <div class="col-12 col-lg-6 offset-lg-6 p-lg-0">  
                             <div class="fables-vision-overlay fables-after-overlay bg-rules">
-                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Who we are</h2>
+                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Retail & Fast-Moving Consumer Goods (FMCG)
+</h2>
                                 <p class="fables-fifth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInRight" data-wow-duration="2s">
-                                Our experience spans diverse industries, locations, and organization sizes. We’ve supported £5B global companies as well as smaller, high-growth businesses.  
-                            Our clients are progressive and ambitious industry disruptors looking to take their business growth to next digital stage. Whether they’re looking for human capital management, supply chain management, or ERP software, we’ve supported organizations like yours.
+                                Consumer demands are shifting at an exponential rate. So how can you keep ahead of the game? And how do you get there before your competitors? Our team has the answer.
+Retail and consumer goods companies must consider ESG, omnichannel business models, and the sustainability and stability of their supply chain if they want to compete in today’s market and retain investment.
+Our retail and consumer goods consultants bring a diverse mix of experience which has been developed via line-roles within the industry in the European, North American and APAC markets. Connect with us today to find out how we can help you.
+How we can help
+Whatever the challenge, our team of experts have the tools to build you bespoke, sustainable and market-leading digital solutions that’ll enable you to stay ahead of the game. 
+
 </p>     
                               </p>
                             </div>
@@ -142,20 +147,14 @@ include 'includes/navigation.php';
                    <div class="row overflow-hidden"> 
                     <div class="col-12 col-lg-6 p-lg-0"> 
                         <div class="fables-vision-overlay fables-after-overlay fables-light-overlay bg-rules">
-                            <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInLeft" data-wow-duration="2s">Our Core Values</h2>
+                            <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInLeft" data-wow-duration="2s">Financial Services
+</h2>
                             <p class="fables-forth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInLeft" data-wow-duration="2s">
                                   
-                              <b>Accountability</b>  <br>
-Individually and collectively, we really care about doing a good job. We sweat every detail but never forget the big picture. We are always trying to improve what we do and how we do it. When we notice something could be better, we do something about it. <br><br>
+                            We have extensive experience in delivering market-leading and transformative solutions across a broad scope of financial services disciplines. Financial services firms need to establish themselves and act as innovators, disruptors, market leaders, and venture capitalists. They need to open doors to organic, sustainable, and strategic growth. 
+How we can help you
+We provide solutions in digital strategy development, product and service innovation, operational, tactile, and digital transformation, and operating models, and regulatory remediation. So, no matter the challenge, our team’s industry-leading expertise, coupled with our global, open innovation network, made up of entrepreneurs, startups, venture capital firms and accelerators, can craft you a bespoke, unique and top-of-the-market solution. Connect with us today so we can get started.
 
-<b>Empathy</b> <br>
-We are a tech business but we take a people-first approach to everything we do. We treat everyone with the respect, consideration, and empathy they deserve. Being kind costs nothing, but it means everything to the experience of our employees, clients, and partners
-Creating a legacy
-We are The Challenger Consultancy. What does that mean? Instigating lasting change, disrupting the ordinary and leaving behind a legacy. <br><br>
-
-<b>Result-Oriented</b> <br>
-Since day 1, our success has been built on exceptional results. This comes from consistently going above and beyond for our clients, our team, and our community.
- 
                               </p>
                             <a href="" class="btn fables-second-border-color fables-second-text-color fables-btn-rouned fables-hover-btn-color font-19 px-5 py-2 position-relative z-index wow fadeInLeft" data-wow-duration="2s"><span>Contact Us</span></a>
                         </div>                    
@@ -165,17 +164,19 @@ Since day 1, our success has been built on exceptional results. This comes from 
              </div>  
          </div> 
      
-         <div class="bg-rules mb-4 mb-lg-0 py-3 py-lg-0" style="background-image: url(assets/custom/enluka_images/hospitality.png)">  
+         <div class="bg-rules mb-4 mb-lg-0 py-3 py-lg-0" style="background-image: url(assets/custom/enluka_images/woman-travel.jpg)">  
                <div class="container-fluid">             
                     <div class="row overflow-hidden">    
                         <div class="col-12 col-lg-6 offset-lg-6 p-lg-0">  
                             <div class="fables-vision-overlay fables-after-overlay bg-rules">
-                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Who We Work With</h2>
+                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Charity & Not-for-Profit
+</h2>
                                 <p class="fables-fifth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInRight" data-wow-duration="2s">
-                                Travel & Hospitality 
-The need for incorporating flexibility and adaptability as cornerstones of company strategy is arguably more prevalent now for the travel & hospitality industry than it has ever been. The travel companies especially are experiencing friction in finding a suitable strategy to satisfy what seems to be an ever-changing market. Those companies looking to get ahead must embed flexibility and adaptability into their customer offering and within their core strategy, as well as remain future-focused.
+                                
+                                Not-for-Profit| Social Enterprise | NGOs
+After a challenging couple of years for organisations in the charity and not-for-profit sector, it’s now more important than ever for firms to understand their audience and adapt accordingly. Our team of experts can help you operate as efficiently as possible while exploiting technology to ensure you’re reaching your key audience groups.
 How we can help you
-Our consultants have been trusted by clients to solve their toughest business challenges. This has included defining strategic roadmaps, developing value propositions, and launching new products and services to market to help clients adapt to the ever-changing landscape. We combine our experience in innovation, technology, branding, UX and strategy consulting with deep technical knowledge to provide bespoke services to our clients across a spectrum of sectors and challenges. Connect with us today to find out how we can help you.
+Our team of industry professionals, experts and entrepreneurs will work with your team to identify problems, then build and implement bespoke, market-leading digital solutions that harness the latest in technology and innovation. Not only are your problems solved, you’ll also have the tools to truly stand out and establish a long relationship with a reliable implementation partner. 
 
                               </p>
                             </div>
@@ -188,7 +189,130 @@ Our consultants have been trusted by clients to solve their toughest business ch
        
             </div> 
     </div>
-      
+
+    <div class="bg-rules mb-4 py-3 py-lg-0" style="background-image: url(assets/custom/images/vision-img.jpg)"> 
+                 <div class="container-fluid">
+                   <div class="row overflow-hidden"> 
+                    <div class="col-12 col-lg-6 p-lg-0"> 
+                        <div class="fables-vision-overlay fables-after-overlay fables-light-overlay bg-rules">
+                            <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInLeft" data-wow-duration="2s">Telecommunications
+</h2>
+                            <p class="fables-forth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInLeft" data-wow-duration="2s">
+                                  
+                            Shifting landscapes are challenging traditional telecommunications business models, making way for new pathways for competitors. This is driving price pressure and an increasing need for firms to differentiate their services. The telecommunications industry has gone through many phases of disruption, but as advancements like fixed wireless and 5G become viable solutions, competitive barriers are beginning to fall.
+How we can help you
+We ensure that any solution is not only successful, but also truly differentiates you in the market. Let us help you navigate this ever-changing industry, together. Connect with us today.
+
+                              </p>
+                            <a href="" class="btn fables-second-border-color fables-second-text-color fables-btn-rouned fables-hover-btn-color font-19 px-5 py-2 position-relative z-index wow fadeInLeft" data-wow-duration="2s"><span>Contact Us</span></a>
+                        </div>                    
+                    </div>
+
+                </div>
+             </div>  
+         </div> 
+     
+         <div class="bg-rules mb-4 mb-lg-0 py-3 py-lg-0" style="background-image: url(assets/custom/enluka_images/woman-travel.jpg)">  
+               <div class="container-fluid">             
+                    <div class="row overflow-hidden">    
+                        <div class="col-12 col-lg-6 offset-lg-6 p-lg-0">  
+                            <div class="fables-vision-overlay fables-after-overlay bg-rules">
+                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Energy, Resources & Utilities
+</h2>
+                                <p class="fables-fifth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInRight" data-wow-duration="2s">
+                                
+                                With oil and gas prices reaching their highest levels in years, the current state of the industry delivered a shock to the utility industry and raises long-term questions about how consumers choose to heat their homes. The increase in digitalisation and the use of advanced data analytics is prospected to grow to transform the chemical industry. These tools will help drive innovation, productivity and profitability, giving businesses an advantage.
+How we can help you
+At Enluka we can help create the innovative edge that will put your businesses in front of competitors in this time of change. Connect with us today to find out how.
+
+                              </p>
+                            </div>
+
+                        </div>
+                    </div>
+              </div>
+            </div>
+    
+       
+            </div> 
+    </div>
+     
+    <div class="bg-rules mb-4 py-3 py-lg-0" style="background-image: url(assets/custom/images/vision-img.jpg)"> 
+                 <div class="container-fluid">
+                   <div class="row overflow-hidden"> 
+                    <div class="col-12 col-lg-6 p-lg-0"> 
+                        <div class="fables-vision-overlay fables-after-overlay fables-light-overlay bg-rules">
+                            <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInLeft" data-wow-duration="2s">Healthcare & Pharmaceuticals
+</h2>
+                            <p class="fables-forth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInLeft" data-wow-duration="2s">
+                                  
+                            Health Administration | Pharmaceuticals| Medical Equipment & Devices |Biotech
+In the last decade, both public and private healthcare systems have been required to adapt and innovate within strict time frames. Increased pressure has been applied to the global healthcare sector’s operations, infrastructure, supply chains and workforces, revealing social disparities in health and the provision of care. Healthcare providers are tasked with proactively addressing these challenges to balance cost optimisation and offering the best service to their patients.
+ How we can help you
+Our team of consultants have the tools to build you bespoke, sustainable and market-leading solutions from defining new and disruptive strategies, to building new improving business operating model, to help our clients achieve the seemingly impossible at a rapid pace.
+
+                              </p>
+                            <a href="" class="btn fables-second-border-color fables-second-text-color fables-btn-rouned fables-hover-btn-color font-19 px-5 py-2 position-relative z-index wow fadeInLeft" data-wow-duration="2s"><span>Contact Us</span></a>
+                        </div>                    
+                    </div>
+
+                </div>
+             </div>  
+         </div> 
+     
+         <div class="bg-rules mb-4 mb-lg-0 py-3 py-lg-0" style="background-image: url(assets/custom/enluka_images/woman-travel.jpg)">  
+               <div class="container-fluid">             
+                    <div class="row overflow-hidden">    
+                        <div class="col-12 col-lg-6 offset-lg-6 p-lg-0">  
+                            <div class="fables-vision-overlay fables-after-overlay bg-rules">
+                                <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInRight" data-wow-duration="2s">Business IT Services
+</h2>
+                                <p class="fables-fifth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInRight" data-wow-duration="2s">
+                                
+                                Private and Public Service Digital Solution Implementation
+Business services companies are evolving with the adoption of new technologies and processes. Automation is a powerful catalyst for effective delivery to customers and efficient operations. But as with all technologies that enter and sustain in the market, this is becoming a hygiene factor. AI will drive the future. Through technologies such as natural language processing we are already seeing radical acceleration of touchless processing for contract management, insurance claims processing, proposal development and work scheduling in industrial and engineering support services environments.
+How we can help you
+Through our business consulting services, we ensure that our clients stay relevant in their business service segment by remaining loyal to a core truth; you can’t out-innovate the market. Get in touch with our team today to find out how we can help you.
+
+                              </p>
+                            </div>
+
+                        </div>
+                    </div>
+              </div>
+            </div>
+    
+       
+            </div> 
+    </div>
+    <div class="bg-rules mb-4 py-3 py-lg-0" style="background-image: url(assets/custom/images/vision-img.jpg)"> 
+                 <div class="container-fluid">
+                   <div class="row overflow-hidden"> 
+                    <div class="col-12 col-lg-6 p-lg-0"> 
+                        <div class="fables-vision-overlay fables-after-overlay fables-light-overlay bg-rules">
+                            <h2 class="fables-second-text-color my-0 font-30 font-weight-bold position-relative z-index wow fadeInLeft" data-wow-duration="2s">Travel & Hospitality
+</h2>
+                            <p class="fables-forth-text-color position-relative z-index mt-4 mb-4 mb-md-5 wow fadeInLeft" data-wow-duration="2s">
+                            The need for incorporating flexibility and adaptability as cornerstones of company strategy is arguably more 
+prevalent now for the travel & hospitality industry than it has ever been. <br><br>The travel companies especially 
+are experiencing friction in finding a suitable strategy to satisfy what seems to be an ever-changing market. 
+Those companies looking to get ahead must embed flexibility and adaptability into their customer offering and 
+within their core strategy, as well as remain future-focused. <br><br>
+<b>How we can help you</b> <br>
+Our consultants have been trusted by clients to solve their toughest business challenges. This has included defining strategic roadmaps,
+developing value propositions, and launching new products and services to market to help clients adapt to the ever-changing landscape. 
+We combine our experience in innovation, technology, branding, UX and strategy consulting with deep technical knowledge 
+to provide bespoke services to our clients across a spectrum of sectors and challenges. <br><br>Connect with us today to find out how we can help you.
+
+                              </p>
+                            <a href="" class="btn fables-second-border-color fables-second-text-color fables-btn-rouned fables-hover-btn-color font-19 px-5 py-2 position-relative z-index wow fadeInLeft" data-wow-duration="2s"><span>Contact Us</span></a>
+                        </div>                    
+                    </div>
+
+                </div>
+             </div>  
+         </div>  
+
     <div class="container-fluid"> 
            <div class="row overflow-hidden">
                <div class="col-12 col-sm-6 p-sm-0 mb-3 mb-md-0 image-container translate-effect-right wow fadeInLeft" data-wow-durationn="2.5s">
