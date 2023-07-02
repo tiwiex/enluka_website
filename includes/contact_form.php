94eeff89b3e068f8352e8ea9d93532f66971ccf9
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-02 12:16:16 +0100

    Added error messages in contact form and sendmailer echos

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -13,9 +13,9 @@
                       </div>   
                       <div class="my-3">
                          <!-- /End Top Header -->
-                <!-- <div class="loading">Loading</div>
+                <div class="loading">Loading</div>
                 <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div> -->
+                <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>                    
                       <button type="submit" class="btn fables-second-background-color rounded-0 text-white btn-block p-3">Send</button>
                     </form>
