44b3ffe9571208ecac5a140251cac53c64e92e04
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-04 19:28:00 +0100

    Updated footers wiyth link again

--- includes/footers.php
+++ includes/footers.php
@@ -44,7 +44,7 @@ _
                     <li><a href="about">About Us</a></li>
                     <li><a href="gallery.html">Services</a></li>
                     <li><a href="team.html">Industries</a></li>
-                    <li><a href="contact_us">Contact Us</a></li>
+                    <li> <a href="contact_us">Contact Us</a></li>
                 </ul>
             </div>
                       
