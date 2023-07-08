42f9b77343821882ec6ead3e48e3abdc790df2c0
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-08 21:52:41 +0100

    Added history to include folder

--- about.php
+++ about.php
@@ -200,51 +200,11 @@ Since day 1, our success has been built on exceptional results. This comes from 
            
        </div>
 
-    <div class="fables-history-carousel">  
-      <br><br>
-<hr>
-                        <div id="sync1" class="owl-carousel owl-theme">
-                  
-                              
-                             
-                             
-                             
-                              <div class="item"> 
-                                   <div class="row">
-                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
-                                           <div class="card">
-                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/focused-work.jpeg" alt="Fables Template">
-                                              <div class="card-body">
-                                                <h5 class="card-title fables-second-text-color">2022</h5>
-                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
-                                              </div>
-                                            </div>
-                                       </div>
-                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
-                                           <div class="card">
-                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/camera-pic.jpeg" alt="Fables Template">
-                                              <div class="card-body">
-                                                <h5 class="card-title fables-second-text-color">2022</h5>
-                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
-                                              </div>
-                                            </div>
-                                       </div>
-                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
-                                           <div class="card">
-                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/project-management.jpeg" alt="Fables Template">
-                                              <div class="card-body">
-                                                <h5 class="card-title fables-second-text-color">2022</h5>
-                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
-                                              </div>
-                                            </div>
-                                       </div>
-                                   </div> 
-                              </div>  
-                              
-                        </div>
-                </div>
-           </div>
-       </div>
+
+Taiwo
+<?php
+include 'includes/history.php';
+?>
 <!-- /End page content -->
     
 <!-- Start Footer 2 Background Image  -->
---
+++ includes/history.php
@@ -0,0 +1,47 @@
+
+    Tiwiex
+    <div class="fables-history-carousel">  
+      <br><br>
+<hr>
+                        <div id="sync1" class="owl-carousel owl-theme">
+                  
+                              
+                             
+                             
+                             
+                              <div class="item"> 
+                                   <div class="row">
+                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
+                                           <div class="card">
+                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/focused-work.jpeg" alt="Fables Template">
+                                              <div class="card-body">
+                                                <h5 class="card-title fables-second-text-color">2022</h5>
+                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
+                                              </div>
+                                            </div>
+                                       </div>
+                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
+                                           <div class="card">
+                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/camera-pic.jpeg" alt="Fables Template">
+                                              <div class="card-body">
+                                                <h5 class="card-title fables-second-text-color">2022</h5>
+                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
+                                              </div>
+                                            </div>
+                                       </div>
+                                       <div class="col-12 col-sm-4 mb-4 mb-md-0">
+                                           <div class="card">
+                                              <img class="card-img-top rounded-0 w-100" src="assets/custom/enluka_images/project-management.jpeg" alt="Fables Template">
+                                              <div class="card-body">
+                                                <h5 class="card-title fables-second-text-color">2022</h5>
+                                                <p class="card-text fables-forth-text-color d-sm-none d-md-block font-15">It is a long established fact that a reader will be distracted by the readable content of a page when </p> 
+                                              </div>
+                                            </div>
+                                       </div>
+                                   </div> 
+                              </div>  
+                              
+                        </div>
+                </div>
+           </div>
+       </div>
