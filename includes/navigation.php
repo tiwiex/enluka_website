68667dbf861bd939823ea753ed89da57274805a8
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-09 07:31:56 +0100

    Trying to update the logo

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,7 +23,7 @@
          
                             <a class="navbar-brand pl-0" href="index"><img src="assets/custom/images/enluka2.png" alt="Fables Template" class="fables-logo"></a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#fablesNavDropdown" aria-controls="fablesNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="fables-iconmenu-icon text-white font-16"></span>
+                                <span class="fables-iconmenu-icon text-white font-16"> </span>
                             </button>
                             <div class="collapse navbar-collapse" id="fablesNavDropdown"> 
 
