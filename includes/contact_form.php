53fe8b3c2fa93d1b0c9a974897becfd94b2d561b
Taiwo Akinosho <tiwiex@yahoo.com>
2023-07-15 16:42:34 +0100

    Removed default form values
    Noticed the validate doesn't work yet.

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -2,13 +2,13 @@
       <h2 class="font-30 font-weight-bold fables-second-text-color">Let's Talk</h2>
       <form method="POST" action="includes/send_mailer.php" class="fables-contact-form">
                       <div class="form-group"> 
-                        <input type="text" name="name" class="form-control rounded-0 p-3"  placeholder="Name" value="Taiwo">   
+                        <input type="text" name="name" class="form-control rounded-0 p-3"  placeholder="Name">   
                       </div>
                       <div class="form-group"> 
-                        <input type="email" name="email" class="form-control rounded-0 p-3" placeholder="Email" value="tiwiex@gmail.com">
+                        <input type="email" name="email" class="form-control rounded-0 p-3" placeholder="Email">
                       </div>
                       <div class="form-group"> 
-                        <input type="text" name="subject" class="form-control rounded-0 p-3" placeholder="Subject" value="Clockwork Orange">   
+                        <input type="text" name="subject" class="form-control rounded-0 p-3" placeholder="Subject">   
                       </div>
                       <div class="form-group"> 
                           <textarea class="form-control rounded-0 p-3" name="message" placeholder="Message" rows="3"></textarea>
